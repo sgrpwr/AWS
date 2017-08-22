2cfefa1f8a4e9e5949d38c6040b976b8fc7e872e
sgrpwr <pawarsgr18@gmail.com>
2017-08-22 18:49:47 +0530

    Added a readLine Code

---
+++ ReadLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Please enter your Name :");
+        string userName = Console.ReadLine();
+        //Concatination has been used below
+        //Console.WriteLine("Hello " + userName);
+        
+        //Place Holder has been used below
+        Console.WriteLine("Hello {0}", userName);
+        Console.ReadKey();
+    }
+}
+
