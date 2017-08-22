a2fe134902dbe91db0771f0ed49bf1442368b4ca
sgrpwr <pawarsgr18@gmail.com>
2017-08-22 17:59:04 +0530

    Added a line

--- HeyThere/Program.cs
+++ HeyThere/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hey There..");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
