0e12f37e982626efe3f1908313d7eb45988ce0b5
sgrpwr <pawarsgr18@gmail.com>
2017-08-27 20:52:47 +0530

    Added two more programs from class

---
+++ AreaOfTriangle/AreaOfTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the sides(a,b,c) of a triangle  :");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        double s = (a + b + c) / 2;
+        double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        Console.WriteLine(area);
+        if (a == b && b == c)
+        {
+            Console.WriteLine("This is an Equilateral trangle and Area is {0} :", area);
+        }
+        else if (a == b && b != c || a != b && b == c)
+        {
+            Console.WriteLine("This is an Isosceles trangle and Area is {0} :", area);
+        }
+        else
+        {
+            Console.WriteLine("This is an Scalene trangle and Area is {0} :", area);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int i, n, fact = 1;
+        Console.WriteLine("Enter a any number to find it's factotial  :");
+        n = Convert.ToInt32(Console.ReadLine());
+        for (i = 1; i <= n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.WriteLine(fact);
+        Console.ReadKey();
+    }
+}
