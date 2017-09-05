a3390efd2d1fbfaf0bc87ca43595d04ccceea23d
sgrpwr <pawarsgr18@gmail.com>
2017-09-05 15:10:17 +0530

    Polymorphism

---
+++ 7. Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PolymorphismApplication
+{
+	class Printdata
+	{
+		void print(int i)
+		{
+			Console.WriteLine("Printing int: {0}", i);
+		}
+
+		void print(double f)
+		{
+			Console.WriteLine("Printing float: {0}", f);
+		}
+
+		void print(string s)
+		{
+			Console.WriteLine("Printing string: {0}", s);
+		}
+		static void Main(string[] args)
+		{
+			Printdata p = new Printdata();
+
+			// Call print to print integer
+			p.print(9);
+
+			// Call print to print float
+			p.print(233.421);
+
+			// Call print to print string
+			p.print("String message");
+			Console.ReadKey();
+		}
+	}
+}
