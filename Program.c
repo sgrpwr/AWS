6578140947f957c5f82c9fba74168abb3940c28b
sgrpwr <pawarsgr18@gmail.com>
2017-09-07 19:43:54 +0530

    conditional code

--- Program.c
+++ Program.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    float p,dis;
-    printf("Enter the amount of purchased item:");
-    scanf("%f",&p);
-    if (p>=1000){
-        p = p-(p*10)/100;
-        printf("Total Price is %f\n",p);}
-    else
-        printf("Total Price is %f",p);
+    float gs,bs;
+    printf("Enter your Basic Salary:\n");
+    scanf("%f",&bs);
+    if (bs<1500){
+        gs = bs+(bs/10)+(bs*9)/10; //BS+HRA+DA
+        printf("Your gross salary is %f:",gs);}
+    else{
+        gs = bs+500+(bs*9.8)/10;   //BS+HRA+DA
+        printf("Your gross salary is %f:",gs);}
     return 0;
 }
 
