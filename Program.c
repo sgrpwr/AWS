d3394d3be0071119db2f93f36638c7e3ec538e62
sgrpwr <pawarsgr18@gmail.com>
2017-09-08 00:40:55 +0530

    %5 sum

--- Program.c
+++ Program.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, sumo = 0,sume=0;
-    
-    printf("Enter a positive integer: ");
+    int n,i,sum5=0,sumo=0;
+    printf("Enter an integer: ");
     scanf("%d",&n);
-    
-    for(i=1; i <= n; ++i)
-    {
-        if(i%2==0){
-            sume += i;   // sum = sum+i;
-        }
-        else{
-            sumo += i;
-        }
-        
+    for(i=1;i<=n;++i){
+        if(i%5==0) sum5+=i;
+        else sumo+=i;
     }
-    
-    printf("Sum odd = %d and Sum even = %d",sumo,sume);
-    
+    printf("sum of d5=%d and sumo=%d",sum5,sumo);
     return 0;
 }
