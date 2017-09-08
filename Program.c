ff389b705256c6acf49bcd3606225b9eb3a13601
sgrpwr <pawarsgr18@gmail.com>
2017-09-08 19:00:11 +0530

    Splitting number

--- Program.c
+++ Program.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,sum5=0,sumo=0;
-    printf("Enter an integer: ");
-    scanf("%d",&n);
-    for(i=1;i<=n;++i){
-        if(i%5==0) sum5+=i;
-        else sumo+=i;
+    int i,num,a[5];
+    printf("Enter a number:");
+    scanf("%d",&num);
+    for(i=2;i>=0;i--){
+        a[i]=num%10;
+        num/=10;
     }
-    printf("sum of d5=%d and sumo=%d",sum5,sumo);
+    for(i=2;i>=0;i--){
+        printf("Splitted %dst element is %d\n",i,a[i]);
+    }
     return 0;
 }
