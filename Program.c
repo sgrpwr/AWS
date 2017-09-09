510493812b58876f01f551d1bae6de9fa3470d9c
sgrpwr <pawarsgr18@gmail.com>
2017-09-09 19:59:24 +0530

    * for 5 times reverse stair

--- Program.c
+++ Program.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
-int main()
-{
-    int i,j;
-    for(i=1;i<=5;i++){
-        for(j=5;j>=1;j--){
+#include<stdio.h>
+int main(){
+    int i,j,k,n=7;
+    for(i=n;i>=1;i--){
+        for(j=1;j<i;j++){
             printf(" ");
-            if(i>=j){
-                printf("*");
-                if(j==1){
-                    printf("\n");
-                    break;
-                }
-            }
         }
+        for(k=n;k>=i;k--){
+            printf("*");
+        }
+        printf("\n");
     }
     return 0;
 }
