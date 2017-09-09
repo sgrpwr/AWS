7f8fd6976ccff23e8f445d9ffb397b449e7879e2
sgrpwr <pawarsgr18@gmail.com>
2017-09-09 18:27:06 +0530

    * for 5 times reverse

--- Program.c
+++ Program.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
-int main()
-{
+#include<stdio.h>
+int main(){
     int i,j;
     for(i=1;i<=5;i++){
-        for(j=1;j<=5;j++){
+        for(j=5;j>=1;j--){
             printf("*");
-            if(i<=j){
+            if(i==j){
                 printf("\n");
                 break;
             }
         }
+        
     }
     return 0;
 }
