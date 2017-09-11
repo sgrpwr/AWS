c42caf760e06fa47ed4b95447a772fe3b286573f
sgrpwr <pawarsgr18@gmail.com>
2017-09-11 21:16:02 +0530

    array sum of very long ints

--- Program.c
+++ Program.c
@@ -6,15 +6,23 @@
 #include <limits.h>
 #include <stdbool.h>
 
-int main()
-{
-    int sum=0,i,n;
-    scanf("%d",&n);
-    int arr[n];
-    for(i=0;i<n;i++)
-        scanf("%d",&arr[i]);
-    for(i=0;i<n;i++){
-        sum=sum+arr[i];}
-    printf("%d",sum);
+long int aVeryBigSum(int n, int ar_size, long int* ar) {
+    // Complete this function
+    long long int sum=0;
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        sum+=ar[ar_i];
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    scanf("%i", &n);
+    long int *ar = malloc(sizeof(long int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        scanf("%li",&ar[ar_i]);
+    }
+    long int result = aVeryBigSum(n, n, ar);
+    printf("%ld\n", result);
     return 0;
 }
