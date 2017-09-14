96218f2d7c7d3c159d02b7bb078370850127d303
sgrpwr <pawarsgr18@gmail.com>
2017-09-14 20:17:18 +0530

    Basic code to struct

--- Program.c
+++ Program.c
@@ -1,38 +1,18 @@
 #include <stdio.h>
 
+struct student{
+    char name[20];
+    int age;
+    int roll_no;
+}s1,s2;
+
 int main()
 {
-    int n=5,i,j,max=0,min=0;
-    int sum[n];
-    int arr[n][n],inp[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&inp[i]);
-    }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            arr[i][j]=inp[j];
-        }
-    }
-    sum[0]=0;
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i==j) continue;
-            sum[i]=sum[i]+arr[i][j];
-        }
-    }
-    
-    max=sum[0];
-    min=sum[0];
-    for(i=1;i<n;i++){
-        if(max<sum[i]) max=sum[i];
-        if(min>sum[i]) min=sum[i];
-    }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%d\n",arr[i][j]);
-        }
-    }
-    //for(i=0;i<n;i++) printf("%d\n",sum[i]);
-    printf("%d  %d",min,max);
+    struct student person;
+    s1.age=22;
+    s1.roll_no=13;
+    s2=s1;
+    printf("%d\n%d\n\n",s1.age,s1.roll_no);
+    printf("%d\n%d",s2.age,s2.roll_no);
     return 0;
 }
