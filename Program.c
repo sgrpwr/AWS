cc25108646f18f8ca3793fdc8ebd9aa7686fb16b
sgrpwr <pawarsgr18@gmail.com>
2017-09-16 01:02:21 +0530

    Kangaroo problem

--- Program.c
+++ Program.c
@@ -1,24 +1,33 @@
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
-int main(){
-    int T,rem,i;
-    scanf("%d",&T);
-    int grade[T];
-    for(i=0;i<T;i++){
-        scanf("%d",&grade[i]);
+char* kangaroo(int x1, int v1, int x2, int v2) {
+    int sum1,sum2,i;
+    sum1=x1;
+    sum2=x2;
+    for(i=0;i<10000;i++){
+        sum1=sum1+v1;
+        sum2=sum2+v2;
+        if(sum1==sum2) break;
     }
     
-    for(i=0;i<T;i++){
-        if(grade[i]<38) grade[i]=grade[i];
-        if(grade[i]>=38){
-            if(grade[i]%5>=3){
-                rem = grade[i]%5;
-                grade[i]=grade[i]+(5-rem);
-            }
-        }
-    }
-    for(i=0;i<T;i++){
-        printf("%d\n",grade[i]);
-    }
+    if(sum1==sum2) return ("YES");
+    else return("NO");
+}
+
+int main() {
+    int x1;
+    int v1;
+    int x2;
+    int v2;
+    scanf("%i %i %i %i", &x1, &v1, &x2, &v2);
+    int result_size;
+    char* result = kangaroo(x1, v1, x2, v2);
+    printf("%s\n", result);
     return 0;
 }
