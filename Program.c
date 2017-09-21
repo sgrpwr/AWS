fc7dc7e6c72db2a78dcfdc82fde9696ea0a21e04
sgrpwr <pawarsgr18@gmail.com>
2017-09-21 16:47:35 +0530

    sum pair complete code :)

--- Program.c
+++ Program.c
@@ -1,18 +1,33 @@
-#include<stdio.h>
-int main(){
-    long long int n,k,i,j,count=0;
-    scanf("%lld %d",&n,&k);
-    int arr[n];
-    int sum[100];
-    for(i=0;i<n;i++) scanf("%d",arr[i]);
-    for(i=0;i<n;i++)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int divisibleSumPairs(int n, int k, vector <int> ar) {
+    int i,j,count=0;
+    int temp[15];
+    for(i=0;i<n;i++){
         for(j=0;j<n;j++){
-            sum[i]=0;
+            temp[i]=0;
             if(i<j){
-                sum[i]=arr[i]+arr[j];
-                if(sum[i]%k==0) count++;
+                temp[i]=ar[i]+ar[j];
+                //cout<<temp[i];
+                if(temp[i]%k==0)
+                    count++;
             }
         }
-    printf("%d",count);
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> ar(n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        cin >> ar[ar_i];
+    }
+    int result = divisibleSumPairs(n, k, ar);
+    cout << result << endl;
     return 0;
 }
