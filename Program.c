dff8f5e8fd114212247316d4e1e7e32a8bb04456
sgrpwr <pawarsgr18@gmail.com>
2017-09-21 17:28:43 +0530

    Apple and Orange from HR

--- Program.c
+++ Program.c
@@ -1,33 +1,37 @@
-#include <bits/stdc++.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
-using namespace std;
-
-int divisibleSumPairs(int n, int k, vector <int> ar) {
-    int i,j,count=0;
-    int temp[15];
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            temp[i]=0;
-            if(i<j){
-                temp[i]=ar[i]+ar[j];
-                //cout<<temp[i];
-                if(temp[i]%k==0)
-                    count++;
-            }
-        }
-    }
-    return count;
-}
-
-int main() {
+int main(){
+    int s,count1=0,count2=0,i;
+    int t;
+    scanf("%d %d",&s,&t);
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    int m;
     int n;
-    int k;
-    cin >> n >> k;
-    vector<int> ar(n);
-    for(int ar_i = 0; ar_i < n; ar_i++){
-        cin >> ar[ar_i];
+    scanf("%d %d",&m,&n);
+    int *apple = malloc(sizeof(int) * m);
+    for(int apple_i = 0; apple_i < m; apple_i++){
+        scanf("%d",&apple[apple_i]);
     }
-    int result = divisibleSumPairs(n, k, ar);
-    cout << result << endl;
+    int *orange = malloc(sizeof(int) * n);
+    for(int orange_i = 0; orange_i < n; orange_i++){
+        scanf("%d",&orange[orange_i]);
+    }
+    for(i = 0; i < m; i++){
+        apple[i]=a+apple[i];
+        if(apple[i]>=s&&apple[i]<=t) count1++;
+    }
+    for(i = 0; i < n; i++){
+        orange[i]=b+orange[i];
+        if(orange[i]>=s&&orange[i]<=t) count2++;
+    }
+    printf("%d\n%d",count1,count2);
     return 0;
 }
