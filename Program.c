f1439241bb4b2ba863f6a62b2c5e0e7e88b817c5
sgrpwr <pawarsgr18@gmail.com>
2017-09-23 15:26:50 +0530

    Bon Appetit complete from HR

--- Program.c
+++ Program.c
@@ -1,24 +1,40 @@
-#include<stdio.h>
-int main(){
-    int n,i,count1=0,count2=0;
-    scanf("%d",&n);
-    int arr[n];
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int bonAppetit(int n, int k, int b, int ar_size, int* ar) {
+    int i,result,sum=0;
+    ar[k]=0;
     for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+        sum+=ar[i];
     }
-    int max=arr[0],min=arr[0];
-    for(i=0;i<n;i++){
-        if(max<arr[i]){
-            max=arr[i];
-            count1++;
-        }
+    //printf("%d",sum);
+    if(sum>b){
+        result=b-(sum/2);
+        return result;
     }
-    for(i=1;i<n;i++){
-        if(min>arr[i]){
-            min=arr[i];
-            count2++;
-        }
+    else return 0;
+    
+}
+
+int main() {
+    int n;
+    int k;
+    char name[25]="Bon Appetit";
+    scanf("%i %i", &n, &k);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        scanf("%i",&ar[ar_i]);
     }
-    printf("%d %d",count1,count2);
+    int b;
+    scanf("%i", &b);
+    int result = bonAppetit(n, k, b, n, ar);
+    if(result==0)
+        printf("%s\n", name);
+    else printf("%d\n", result);
     return 0;
 }
