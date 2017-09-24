f96c1d5d0b983675964f2cf1432d5fcba25c5d39
sgrpwr <pawarsgr18@gmail.com>
2017-09-24 12:49:12 +0530

    Birthday chocolate not complete from HR

--- Program.c
+++ Program.c
@@ -6,35 +6,28 @@
 #include <limits.h>
 #include <stdbool.h>
 
-int bonAppetit(int n, int k, int b, int ar_size, int* ar) {
-    int i,result,sum=0;
-    ar[k]=0;
-    for(i=0;i<n;i++){
-        sum+=ar[i];
+int solve(int n, int s_size, int* s, int d, int m){
+    int i,j,count=0,sum=0;
+    for(i=0;i<5;i++){
+        for(j=0;j<2;j++){
+            if(s[i]+s[j+1]==d)
+                count++;
+        }
     }
-    //printf("%d",sum);
-    if(sum>b){
-        result=b-(sum/2);
-        return result;
-    }
-    else return 0;
-    
+    return count;
 }
 
 int main() {
     int n;
-    int k;
-    char name[25]="Bon Appetit";
-    scanf("%i %i", &n, &k);
-    int *ar = malloc(sizeof(int) * n);
-    for(int ar_i = 0; ar_i < n; ar_i++){
-        scanf("%i",&ar[ar_i]);
+    scanf("%d", &n);
+    int *s = malloc(sizeof(int) * n);
+    for(int s_i = 0; s_i < n; s_i++){
+        scanf("%d",&s[s_i]);
     }
-    int b;
-    scanf("%i", &b);
-    int result = bonAppetit(n, k, b, n, ar);
-    if(result==0)
-        printf("%s\n", name);
-    else printf("%d\n", result);
+    int d;
+    int m;
+    scanf("%d %d", &d, &m);
+    int result = solve(n, n, s, d, m);
+    printf("%d\n", result);
     return 0;
 }
