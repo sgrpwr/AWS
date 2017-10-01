2088ede7ecd32d579a97edce626ee9d5866ee9ae
sgrpwr <pawarsgr18@gmail.com>
2017-10-01 21:49:14 +0530

    Migratory Birds from HR

--- Program.c
+++ Program.c
@@ -6,30 +6,31 @@
 #include <limits.h>
 #include <stdbool.h>
 
-int solve(int n, int s_size, int* s, int d, int m){
-    int i,j,count=0,sum;
+int migratoryBirds(int n,int* ar) {
+    int i,j,max=0,count[n],loc=0;
     for(i=0;i<n;i++){
-        sum=0;
-        for(j=i;j<i+m;j++){
-            sum+=s[j];}
-        if(sum==d)
-            count++;
-        
+        count[i]=0;
+        for(j=0;j<n;j++){
+            if(ar[i]==ar[j])
+                count[i]++;
+        }
     }
-    return count;
+    
+    for(i=0;i<n;i++) printf("%d\n",count[i]);
+    for(i=0;i<n;i++) if(max<count[i]){ max=count[i]; loc=i;}
+    printf("\nMax:%d\nLoc:%d",count[loc],loc);
+    return (ar[loc]);
 }
 
 int main() {
     int n;
-    scanf("%d", &n);
-    int *s = malloc(sizeof(int) * n);
-    for(int s_i = 0; s_i < n; s_i++){
-        scanf("%d",&s[s_i]);
+    scanf("%i", &n);
+    int *ar = malloc(sizeof(int) * n);
+    for(int ar_i = 0; ar_i < n; ar_i++){
+        scanf("%i",&ar[ar_i]);
     }
-    int d;
-    int m;
-    scanf("%d %d", &d, &m);
-    int result = solve(n, n, s, d, m);
-    printf("%d\n", result);
+    int result = migratoryBirds(n, ar);
+    printf("%d", result);
     return 0;
 }
+
